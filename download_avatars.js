db4977bb42efafe3a34747a957be6471aea86046
Steven Bamford <sbamford@alumni.ubc.ca>
2017-01-16 23:03:21 +0000

    Created function that downloads img and stores in avatar directory

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var fs = require("fs");
 
 var GITHUB_USER = "stevenbamford";
 var GITHUB_TOKEN = "dbd8fb3e963b83659c857a40cd77fcd00fd7ccb1";
@@ -25,3 +26,18 @@ getRepoContributors("jquery", "jquery", function(err, result){
    })
 
    });
+
+
+function downloadImageByURL(url, filePath) {
+
+if(fs.existsSync("./avatars") !== true){
+  fs.mkdirSync("./avatars");
+}
+  request.get(url)
+       .on("error", function(err){
+          console.log("errors: ", err);
+       })
+       .pipe(fs.createWriteStream("./avatars/" + filePath));
+}
+
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "kvirani.jpg");
