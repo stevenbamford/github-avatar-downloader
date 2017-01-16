d51b4b81cd02bc5ac59210dbfa72b0eb6170cd71
Steven Bamford <sbamford@alumni.ubc.ca>
2017-01-16 21:41:00 +0000

    Built URL with github username and access token

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,13 @@
 var request = require('request');
 
+var GITHUB_USER = "stevenbamford";
+var GITHUB_TOKEN = "dbd8fb3e963b83659c857a40cd77fcd00fd7ccb1";
+
 function getRepoContributors(repoOwner, repoName, cb) {
 
-  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName +"/contributors";
-  request.get(url);
+  var requestURL = "https://" + GITHUB_USER + ":" + GITHUB_TOKEN + "@api.github.com/repos/" + repoOwner + "/" + repoName +"/contributors";
+  console.log(requestURL);
+  // request.get(url);
 }
 
 getRepoContributors("jquery", "jquery", function(err, result){
