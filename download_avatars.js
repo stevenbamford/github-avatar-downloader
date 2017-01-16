d265f39ea33cb2ed2f1340cf2fcd45a967b98c2f
Steven Bamford <sbamford@alumni.ubc.ca>
2017-01-16 22:15:38 +0000

    Function successfully makes GET request and receives JSON data back. Iterated through JSON to log avatar urls of contributors

--- download_avatars.js
+++ download_avatars.js
@@ -6,11 +6,22 @@ var GITHUB_TOKEN = "dbd8fb3e963b83659c857a40cd77fcd00fd7ccb1";
 function getRepoContributors(repoOwner, repoName, cb) {
 
   var requestURL = "https://" + GITHUB_USER + ":" + GITHUB_TOKEN + "@api.github.com/repos/" + repoOwner + "/" + repoName +"/contributors";
-  console.log(requestURL);
-  // request.get(url);
+  var requestOptions = {
+    url: requestURL,
+    headers: {
+    "user-agent": "GitHub Avatar Downloader - Student Project"
+    },
+    method: "GET"
+}
+  request.get(requestOptions, cb);
 }
 
 getRepoContributors("jquery", "jquery", function(err, result){
-  console.log("Errors:", err);
-  console.log("Results:", result);
-});
+   result = JSON.parse(result.body);
+   // console.log(result);
+
+   result = result.forEach(function(user){
+    console.log(user.avatar_url);
+   })
+
+   });
