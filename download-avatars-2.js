daa41d7d8353ae53d18b6174206db2b8228fbf41
Steven Bamford <sbamford@alumni.ubc.ca>
2017-01-17 03:36:53 +0000

    Program now handles common error situations, such as missing/damaged .env file, owner/repo does not exist

--- download-avatars-2.js
+++ download-avatars-2.js
@@ -3,10 +3,21 @@ require('dotenv').config();
 var request = require('request');
 var fs = require("fs");
 var dotenv = require('dotenv').config();
+var GITHUB_USER = process.env.GITHUB_USER
+var GITHUB_TOKEN = process.env.GITHUB_TOKEN
+var userInput = process.argv.slice(2);
 
 function response (err, result){
   if(err){
     console.log("Error: ", err);
+  }
+  if(result.statusCode === 401){
+      console.log("Ensure that github token in .env is entered correctly");
+      return;
+    }
+  if(result.statusCode !== 200){
+    console.log("The provided repo/owner does not exist.");
+    return;
   }
    result = JSON.parse(result.body);
    console.log("Downloading avatars now...");
@@ -21,19 +32,26 @@ function downloadImageByURL(url, filePath) {
     fs.mkdirSync("./avatars");
   }
   request.get(url)
+    .auth(null, null, true, GITHUB_TOKEN)
     .on("error", function(err){
       console.log("errors: ", err);
-   })
+      })
     .pipe(fs.createWriteStream("./avatars/" + filePath));
 }
-
-var GITHUB_USER = process.env.GITHUB_USER
-var GITHUB_TOKEN = process.env.GITHUB_TOKEN
-var userInput = process.argv.slice(2);
 
 function getRepoContributors(repoOwner, repoName, cb) {
   if(userInput.length < 2){
     console.log("Please try again. Enter a GitHub repository owner and name");
+    return;
+  }
+
+  if(fs.existsSync("./.env") !== true){
+    console.log(".env file not found. Please create one.");
+    return;
+  }
+
+  if(!GITHUB_USER || !GITHUB_TOKEN){
+    console.log("Missing github username or token in .env file");
     return;
   }
 
